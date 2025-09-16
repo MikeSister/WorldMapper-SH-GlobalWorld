12d2af2776843f2de4ebfe02dc542716be830c53
Tiantian Fan <Tiantian.fan@walmart.com>
2025-09-16 17:06:29 +0800

    Adjust base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pages/' : '/', // 仅生产环境添加 base
+  base: mode === 'production' ? '/pages/mikesister.github.io/WorldMapper-SH-GlobalWorld/' : '/', // 仅生产环境添加 base
   // ...其他配置
 }));
