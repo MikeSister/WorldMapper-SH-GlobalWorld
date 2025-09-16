ce235fb6b52603a8fcba41b9fef3fe18591d94d3
Tiantian Fan <Tiantian.fan@walmart.com>
2025-09-16 16:53:44 +0800

    Update deployment

--- src/components/PortTooltipRenderer.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOMServer from "react-dom/server";
-import PortTooltip from "./PortTooltip";
-import type { LocationQuantityInfo } from "../type";
-
-export const renderPortTooltip = (props: LocationQuantityInfo): string => {
-  return ReactDOMServer.renderToString(<PortTooltip {...props} />);
-};
